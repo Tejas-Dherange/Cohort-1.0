e9e21552a1da126822cd5a191ac110f943d03fe8
Tejas Dherange <tejasdherange0099@gmail.com>
2025-02-22 18:12:25 +0530

    Add 6-10 challenge

--- 08-week/Challenges/DOM-6_10/challenge-9/app.js
+++ 08-week/Challenges/DOM-6_10/challenge-9/app.js
@@ -1,3 +1,28 @@
 /**
  * Write your challenge solution here
  */
+document.addEventListener("click", (event) => {
+  const panel = document.querySelector(".panel");
+  const toggleButton = document.querySelector(".toggle-btn");
+
+  if (!panel.contains(event.target) && !toggleButton.contains(event.target)) {
+    panel.style.right = "-360px";
+  }
+});
+
+
+document.querySelector(".toggle-btn").addEventListener("click", () => {
+  document.querySelector(".panel").style.right = "0px";
+});
+
+
+document.querySelector(".close-btn").addEventListener("click", () => {
+  document.querySelector(".panel").style.right = "-360px";
+});
+
+document.querySelectorAll(".menu-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    const text = item.innerText;
+    alert(text);
+  });
+});
